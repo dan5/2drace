a0da37ce2d6c76b34c9a2a92f9b3ff73b508f585
dan5 <dan5.ya@gmail.com>
2013-08-21 15:19:36 +0900

    ハンドル画像表示テスト

--- Assets/Scripts/GUIScript.js
+++ Assets/Scripts/GUIScript.js
@@ -2,8 +2,8 @@ var playerObj : GameObject;
 var aTexture : Texture;
 
 function OnGUI () {
-  var w = Screen.width * 0.3f;
-  var x = Screen.width * 2 / 3 - w / 2;
+  var w = Screen.width * 0.4f;
+  var x = Screen.width * 0.72f - w / 2;
   var y = Screen.height - w * 1.5f;
   var pivotPoint : Vector2 = Vector2(x + w / 2, y + w / 2);
   var angleY : float = playerObj.transform.localEulerAngles.y;
